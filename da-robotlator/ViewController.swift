44efd22eb0b41258cfa1364e46869e78a033a7bf
Daniel Esposito <danielesposito@macbkk.com>
2016-03-19 00:52:00 +0700

    Changes to the ViewController.swift file added.

--- da-robotlator/ViewController.swift
+++ da-robotlator/ViewController.swift
@@ -11,12 +11,30 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    
+    enum Operations: String {
+        
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+        
+    }
+    
     //Outlets
     @IBOutlet weak var ledOutputLabel: UILabel!
     
     //Properties
     var btnSound: AVAudioPlayer!
-    var tmpCalcNumber = 0
+    var tmpCalcNumber = ""
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    
+    var currentOperation: Operations = Operations.Empty
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,48 +59,88 @@ class ViewController: UIViewController {
     }
     
     
-    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        
+        
+        processOperation(Operations.Divide)
+        
+        if leftValStr.characters.count > 0 {
+            
+            print("From Divider Action: \(leftValStr)")
+            
+            ledOutputLabel.text = ""
 
+        }
+        
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        
+        processOperation(Operations.Multiply)
+    }
+
+    @IBAction func onSubstractPressed(sender: AnyObject) {
+        
+        processOperation(Operations.Substract)
+    }
+    
+    @IBAction func onAdditionPressed(sender: AnyObject) {
+        
+        processOperation(Operations.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        
+        processOperation(Operations.Equals)
+    }
+    
+    @IBAction func clearLedLabelPressed(btn: UIButton) {
+        
+        ledOutputLabel.text = "0"
+    }
+    
     
     @IBAction func numberPressed(btn: UIButton){
         
-        btnSound.play()
+        playSound()
         
-        switch btn.tag {
-        case 0:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 1:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 2:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 3:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 4:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 5:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 6:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 7:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 8:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-        case 9:
-            showSelectedNumberMessage(btn.tag)
-            addNumberString(btn.tag)
-
-        default: break
-            
-        }
+//        switch btn.tag {
+//        case 0:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 1:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 2:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 3:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 4:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 5:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 6:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 7:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 8:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//        case 9:
+//            showSelectedNumberMessage(btn.tag)
+//            addNumberString(btn.tag)
+//
+//        default: break
+//            
+//        }
+        
+        addNumberString(btn.tag)
         
     }
     
@@ -101,13 +159,34 @@ class ViewController: UIViewController {
         if ledOutputLabel.text!.hasPrefix("0") && ledOutputLabel.text!.characters.count == 1 {
             
             ledOutputLabel.text = ""
+        
         }
+        
         
         ledOutputLabel.text = ledOutputLabel.text! + "\(strKeypad)"
         
+        leftValStr = ledOutputLabel.text!
+        
+        print("LeftValeString: \(leftValStr)")
+        
+        
     }
     
+    func processOperation(op: Operations) {
+        
+        playSound()
+        
+    }
  
+    func playSound() {
+        
+        if btnSound.playing {
+            
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
     
 }
 
